2b861b502ee5951b61cec6c2a470caf6f848bb98
sbpark88 <saebyulpark88@gmail.com>
2024-05-01 19:00:03 +0900

    Close #5: Add default router

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 "use strict";
 
 import("../src/style/main.scss");
+import App from "./App";
+import router from "./routes";
+
+const root = document.querySelector("#root");
+root.append(new App().el);
+
+router();
---
+++ src/routes/Home.js
@@ -0,0 +1,7 @@
+import { Component } from "../core/MyReact";
+
+export default class Home extends Component {
+  render() {
+    this.el.innerHTML = `<h1>Home</h1>`;
+  }
+}
